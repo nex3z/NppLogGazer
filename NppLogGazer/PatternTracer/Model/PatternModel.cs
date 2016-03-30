46fd1f655d9c01d53f5dd0364baf1bc2e125df01
nex3z <litianxing9@gmail.com>
2016-03-30 23:44:34 +0800

    Add title field for pattern model.

--- NppLogGazer/PatternTracer/Model/PatternModel.cs
+++ NppLogGazer/PatternTracer/Model/PatternModel.cs
@@ -11,6 +11,7 @@ namespace NppLogGazer.PatternExtractor.Model
     {
         public List<string> PatternText { get; set; }
         public PatternType Type { get; set; }
+        public string Title { get; set; }
 
         public PatternModel() { }
 
@@ -20,9 +21,20 @@ namespace NppLogGazer.PatternExtractor.Model
             this.Type = type;
         }
 
+        public PatternModel(String title, List<string> pattern, PatternType type)
+        {
+            this.Title = title;
+            this.PatternText = pattern;
+            this.Type = type;
+        }
+
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
+            if (Title != null)
+            {
+                sb.Append(Title);
+            }
             foreach (var pattern in this.PatternText)
             {
                 sb.Append(pattern);
